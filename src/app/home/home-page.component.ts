17fc0d3f5707c9ebe3cbe6636f8d6c524c2351d3
Adam Armour <adam.armour@teamaol.com>
2017-02-25 14:35:53 -0700

    feat(home): add map controls to home page

--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -4,7 +4,12 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   selector: 'mq-home-page',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <mb-map></mb-map>
+    <mb-map>
+      <mb-navigation-control></mb-navigation-control>
+      <mb-container-control>
+        <button md-mini-fab><md-icon>search</md-icon></button>
+      </mb-container-control>
+    </mb-map>
   `
 })
 export class HomePageComponent {

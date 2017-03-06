8c757a29d3b428ac25be0d171d834a6c4da50fb6
Adam Armour <adam.armour@teamaol.com>
2017-03-05 18:07:28 -0700

    style(home): search-result: add styles

--- src/app/home/search/components/search-result/search-result.component.ts
+++ src/app/home/search/components/search-result/search-result.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'mq-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.scss']
+  styleUrls: ['./search-result.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchResultComponent implements OnInit {
 
   @Input() display: string = '';
+
+  @Output() select: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
